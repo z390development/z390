ad3522904f163a98f5afe3ca265c60b3afe3f1b8
Don Higgins <don@higgins.net>
2012-05-21 15:17:00 -0500

    Reconstructed version v1.5.06 of z390 committed on behalf of Don Higgins

---
+++ RT/rtclean2.bat
@@ -0,0 +1,14 @@
+rem remove generated files from %1 except 390's
+erase %1\*.BAK
+erase %1\*.BAL
+erase %1\*.ERR
+erase %1\*.PCH
+erase %1\*.PRN
+erase %1\*.OBJ
+erase %1\*.LST
+erase %1\*.LOG
+erase %1\*.STA
+erase %1\*.TR?
+erase %1\*.XPR
+erase %1\*.TPH
+erase %1\*.XPT
