01289e61229e40c1a4588a6f822fd828430dbc1d
dsh33782 <don@higgins.net>
2021-05-12 09:25:46 -0400

    remove rpi test directory

--- RPI/TSTACAL2.bat
+++
@@ -1,4 +0,0 @@
-cd..
-rem test mz390 acall, aentry, aexit 
-call bat\asm rpi\TSTACAL2 TRACEALL 
-pause verify test ran without errors
--- RPI/test.bat
+++
@@ -1,4 +0,0 @@
-cd..
-rem test z390 instruction 
-call bat\asmlg rpi\test traceall
-pause verify test ran without errors
