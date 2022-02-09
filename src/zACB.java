e97021ea22ef2b0d7ea9c602498bd5e1bb28a737
Abe Kornelis <abe@bixoft.nl>
2022-02-09 11:16:30 +0100

    Fix incorrect copyright notices

--- src/zACB.java
+++ src/zACB.java
@@ -1,23 +1,23 @@
+/*
+z390 - Mainframe assembler emulator and run-time engine
+Copyright (C) 2021 z390 Assembler LLC
+
+This file is part of z390.
+z390 is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+z390 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, see <https://www.gnu.org/licenses/>.
+*/
 public class zACB
 {/***************************************************************************
-  * 
-  * z390 portable mainframe assembler and emulator.
-  * 
-  * Copyright 2016 Abe Kornelis
-  * 
-  * z390 is free software; you can redistribute it and/or modify it under the
-  * terms of the GNU General Public License as published by the Free Software
-  * Foundation; either version 2 of the License, or (at your option) any
-  * later version.
-  * 
-  * z390 is distributed in the hope that it will be useful, but WITHOUT ANY
-  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-  * details.
-  * 
-  * You should have received a copy of the GNU General Public License along
-  * with z390; if not, write to the Free Software Foundation, Inc., 59 Temple
-  * Place, Suite 330, Boston, MA 02111-1307 USA
   * 
   * The zACB object is the Java-level representation of an ACB
   * The zACB class handles all ACB-based requests: Openn and Close
