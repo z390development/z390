82814570fc142e86eb0d1011a58db895041e4890
Abe Kornelis <abe@bixoft.nl>
2022-02-10 16:26:51 +0100

    Fix for Issue #195 (=RPI 1622) Issue S0D3 if any of the privileged instructions are used

--- src/pz390.java
+++ src/pz390.java
@@ -363,6 +363,7 @@ public class pz390 {
      *                         fixed-point-overflow exceptions
      *                    Remove invalid call to get_int_add_cc() in ALSIHN instruction emulation
      * 03/29/17 RPI 2010 Save and restore field psw_ins_len in method trace_psw()
+	 * 2018-07-30 RPI 1622 Issue S0D3 if any of the privileged instructions are used
      * 2019-09-22 RPI 2201 dsh use math.RoundingMode(int) to fix depreciated setScale(int,int)
      * 2019-09-29 RPI 2202 dsh add new instructions documented in SA22-7832-12 dated 2019-09  pg 79 summary
      *   AND WITH COMPLEMENT (NCRK, NCGRK)
@@ -674,6 +675,8 @@ public class pz390 {
 
 	int psw_pic_fp_div = 0x0cf;
 
+	int psw_pic_spec_op = 0x0d3; // RPI 1622 DK Special Operation Exception
+
 	int psw_pic_timeout = 0x422;
     int psw_pic_gm_err = 0x804; // getmain request invalid
 
@@ -824,7 +827,6 @@ public class pz390 {
 	int mf3 = 0;
 
 	int mf4 = 0;
-	
 
 	int[] mask_bits = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
 
@@ -2788,9 +2790,11 @@ public class pz390 {
 		 */
 		switch (opcode1) {
 		case 0x80: // 1530 "8000" "SSM" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x82: // 1540 "8200" "LPSW" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			psw_check = false;
 			ins_setup_s();
 			set_psw_loc(mem.getInt(bd2_loc + 4));
@@ -3040,6 +3044,7 @@ public class pz390 {
 			}
 			break;
 		case 0x99: // 1790 "99" "TRACE" "RS"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rs();
 			break;
 		case 0x9A: // 1800 "9A" "LAM" "RS"
@@ -3115,18 +3120,22 @@ public class pz390 {
 			exec_clcle();
 			break;
 		case 0xAC: // 2520 "AC" "STNSM" "SI"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_si();
 			break;
 		case 0xAD: // 2530 "AD" "STOSM" "SI"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_si();
 			break;
 		case 0xAE: // 2540 "AE" "SIGP" "RS"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rs();
 			break;
 		case 0xAF: // 2550 "AF" "MC" "SI"
 			ins_setup_si();
 			break;
 		case 0xB1: // 2560 "B1" "LRA" "RX"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rx();
 			break;
 		case 0xB2:
@@ -3136,9 +3145,11 @@ public class pz390 {
 			ins_B3XX();
 			break;
 		case 0xB6: // 4430 "B6" "STCTL" "RS"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rs();
 			break;
 		case 0xB7: // 4440 "B7" "LCTL" "RS"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rs();
 			break;
 		case 0xB9:
@@ -3880,6 +3891,7 @@ public class pz390 {
 			sz390.svc(11);
 			break;
 		case 0x07: // 30 "0107" "SCKPF" "E"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_e();
 			break;
 		case 0x0A: // 40 "010A" "PFPO" "E"   RPI 1013
@@ -3924,6 +3936,7 @@ public class pz390 {
 			set_psw_amode(psw_amode31_bit | psw_extended_amode64_bit); // RPI 1506
 			break;
 		case 0xFF: // 80 "01FF" "TRAP2" "E"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_e();
 			break;
 		}
@@ -4196,11 +4209,13 @@ public class pz390 {
 		opcode2 = mem_byte[psw_loc + opcode2_offset_s] & 0xff;
 		switch (opcode2) {
 		case 0x02: // 2570 "B202" "STIDP" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			psw_check = false;
 			ins_setup_s();
 			mem.putLong(bd2_loc, cpu_id);
 			break;
 		case 0x04: // 2580 "B204" "SCK" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x05: // 2590 "B205" "STCK" "S"
@@ -4229,15 +4244,19 @@ public class pz390 {
 			mem.putLong(bd2_loc, last_stck);
 			break;
 		case 0x06: // 2600 "B206" "SCKC" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x07: // 2610 "B207" "STCKC" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x08: // 2620 "B208" "SPT" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x09: // 2630 "B209" "STPT" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x0A: // 2640 "B20A" "SPKA" "S"
@@ -4250,12 +4269,15 @@ public class pz390 {
 			ins_setup_s();
 			break;
 		case 0x10: // 2670 "B210" "SPX" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x11: // 2680 "B211" "STPX" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x12: // 2690 "B212" "STAP" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x18: // 2700 "B218" "PC" "S"
@@ -4271,6 +4293,7 @@ public class pz390 {
 			ins_setup_s();
 			break;
 		case 0x21: // 2730 "B221" "IPTE" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x22: // 2740 "B222" "IPM" "RRE"
@@ -4299,15 +4322,19 @@ public class pz390 {
 			ins_setup_rre();
 			break;
 		case 0x29: // 2810 "B229" "ISKE" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x2A: // 2820 "B22A" "RRBE" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x2B: // 2830 "B22B" "SSKE" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x2C: // 2840 "B22C" "TB" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x2D: // 2850 "B22D" "DXR" "RRE"
@@ -4325,48 +4352,62 @@ public class pz390 {
 			check_lh_div();
 			break;
 		case 0x2E: // 2860 "B22E" "PGIN" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x2F: // 2870 "B22F" "PGOUT" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x30: // 2880 "B230" "CSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x31: // 2890 "B231" "HSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x32: // 2900 "B232" "MSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x33: // 2910 "B233" "SSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x34: // 2920 "B234" "STSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x35: // 2930 "B235" "TSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x36: // 2940 "B236" "TPI" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x37: // 2950 "B237" "SAL" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x38: // 2960 "B238" "RSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x39: // 2970 "B239" "STCRW" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x3A: // 2980 "B23A" "STCPS" "S"
 			ins_setup_s();
 			break;
 		case 0x3B: // 2990 "B23B" "RCHP" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x3C: // 3000 "B23C" "SCHM" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x40: // 3010 "B240" "BAKR" "RRE"
@@ -4398,12 +4439,14 @@ public class pz390 {
 			fp_put_db(rf1, tz390.fp_eh_type, fp_rdv1);
 			break;
 		case 0x46: // 3050 "B246" "STURA" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x47: // 3060 "B247" "MSTA" "RRE"
 			ins_setup_rre();
 			break;
 		case 0x48: // 3070 "B248" "PALB" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x49: // 3080 "B249" "EREG" "RRE"
@@ -4450,6 +4493,7 @@ public class pz390 {
 			}
 			break;
 		case 0x4B: // 3100 "B24B" "LURA" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x4C: // 3110 "B24C" "TAR" "RRE"
@@ -4471,6 +4515,7 @@ public class pz390 {
 			reg.putInt(rf1 + 4,ar_reg.getInt(rf2 >> 1)); // RPI 1055
 			break;
 		case 0x50: // 3150 "B250" "CSP" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x52: // 3160 "B252" "MSR" "RRE"
@@ -4526,6 +4571,7 @@ public class pz390 {
 			ins_setup_rre();
 			break;
 		case 0x76: // 3250 "B276" "XSCH" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x77: // 3260 "B277" "RP" "S"
@@ -4571,6 +4617,7 @@ public class pz390 {
 			psw_cc = psw_cc0;
 			break;
 		case 0x7D: // 3290 "B27D" "STSI" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0x99: // 3300 "B299" "SRNM" "S"
@@ -4620,19 +4667,21 @@ public class pz390 {
 			ins_setup_rre();
 			break;
 		case 0xB0: // 3400 "B2B0" "STFLE" "S" Z9-3
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			psw_check = false;
 			ins_setup_s();
 			/*
 			 * store feature bit list
 			 */
 			mem.putLong(bd2_loc, sz390.get_feature_bits());
-			reg.put(r0 + 7, (byte) 0); // number of feature doulbe words-1
+			reg.put(r0 + 7, (byte) 0); // number of feature double words-1
 			psw_cc = psw_cc0;
 			break;
 		case 0xB1: // 3380 "B2B1" "STFL" "S"
 			ins_setup_s();
 			break;
 		case 0xB2: // 3390 "B2B2" "LPSWE" "S"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;
 		case 0xB8: // 3300 "B2B8" "SRNMB" "S" RPI 1125
@@ -4671,9 +4720,11 @@ public class pz390 {
 			ins_setup_ie();
 			break;		
 		case 0xFC: // "B2FC S 7,72 TABORT D2(B2) z15
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_s();
 			break;		
 		case 0xFF: // 3400 "B2FF" "TRAP4" "S"
+			set_psw_check(psw_pic_spec_op); // RPI 1622 DK Special Operation Exception
 			ins_setup_s();
 			break;
 		}
@@ -6314,6 +6365,7 @@ public class pz390 {
 			reg.putLong(rf1, reg.getLong(rf2));
 			break;
 		case 0x05: // 4500 "B905" "LURAG" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x06: // 4600 "B906" "LGBR" "RRE" Z9-10
@@ -6551,6 +6603,7 @@ public class pz390 {
 					.getLong(rf2));
 			break;
 		case 0x25: // 4760 "B925" "STURG" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x26: // 4880 "B926" "LBR" "RRE" Z9-12
@@ -6564,9 +6617,11 @@ public class pz390 {
 			reg.putInt(rf1 + 4, reg.getShort(rf2 + 6));
 			break;
 		case 0x28: // "B928" "PCKMO" "RRE" RPI 1125
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x29: //  "B929=KMA,54,340", // B929 RRFb 54,340 KMA R1,M3,R2 RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rrfb();
 			break;	
 		case 0x2A: // "B92A" "KMF" "RRE" RPI 1125
@@ -6599,13 +6654,13 @@ public class pz390 {
 			psw_cc = get_long_log_comp_cc(reg.getLong(rf1), (long) reg
 					.getInt(rf2 + 4));
 			break;
-		case 0x39: // B929 RRFa 36,360 DFLTCC R1,R2,R3 RPI 2202
+		case 0x39: // B939 RRFa 36,360 DFLTCC R1,R2,R3 RPI 2202
 			ins_setup_rrfa();
 			break;			
 		case 0x3A: //  "B93A RRE 14,144 KDSA R1,R2 RPI 2202"
 			ins_setup_rre();
 			break;	
-		case 0x3C:  // B93C RRE 14,144 O=PRNO R1,R2 RPI 2202
+		case 0x3C:  // B93C RRE 14,144 PRNO R1,R2 RPI 2202
 			ins_setup_rre();
 			break;	
 		case 0x3E: // 4810 "B93E" "KIMD" "RRE"
@@ -6982,15 +7037,18 @@ public class pz390 {
 			psw_cc = get_long_log_sub_cc();
 			break;
 		case 0x8A: // 4910 "B98A" "CSPG" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x8D: // 4920 "B98D" "EPSW" "RRE"
 			ins_setup_rre();
 			break;
 		case 0x8E: // 4930 "B98E" "IDTE" "RRF"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rrfe();
 			break;
 		case 0x8F: // 	"B98F=CRDTE,54,344", // B98F rrfb 54,344 CRDTE R1,R3,R2[,M4] RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rrfb();
 			break;
 		case 0x90: // 4940 "B990" "TRTT" "RRE"
@@ -7237,6 +7295,7 @@ public class pz390 {
 			ins_setup_rre();
 			break;
 		case 0x9D: // 5040 "B99D" "ESEA" "RRE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0x9E: // 5050 "B99E" "PTI" "RRE"
@@ -7246,21 +7305,27 @@ public class pz390 {
 			ins_setup_rre();
 			break;
 		case 0xA1:  // B9A1 RRE 14,144 TPEI R1,R2 RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;	
 	     case 0xA2:  // 10 "B9A2" "PTF" "RRE" 14 RPI 817
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 	         ins_setup_rre();
 	         break;
-		case 0xAC:  // "B9AC=IRBM,14,144", // B9AC RRE 14,144 IRBM R1,R2 RPI 2202
-				ins_setup_rre();
-				break;	
 		case 0xAA: // 5250 "B9AA" "LPTEA" "RRF-b" Z9-19
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rrf3(); // dsh rpi 2202
 			break;
+		case 0xAC:  // "B9AC=IRBM,14,144", // B9AC RRE 14,144 IRBM R1,R2 RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
+				ins_setup_rre();
+				break;	
 		case 0xAE: // "B9AE" "RRBM" "RRE" RPI 1125 Z196
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rre();
 			break;
 		case 0xAF:  // 20 "B9AF" "PFMF" "RRFc" 39 RPI 817
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 	         ins_setup_RRFc();
 	         break;
 		case 0xB0: // 5070 "B9B0" "CU14" "RRE"
@@ -8200,6 +8265,7 @@ public class pz390 {
 			psw_cc = get_long_comp_cc(rlv1, 0);
 			break;
 		case 0x03: // 5400 "E303" "LRAG" "RXY"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rxy();
 			break;
 		case 0x04: // 5410 "E304" "LG" "RXY"
@@ -8319,6 +8385,7 @@ public class pz390 {
 			psw_cc = get_int_comp_cc(rv1, 0);
 			break;
 		case 0x13: // 5510 "E313" "LRAY" "RXY"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rxy();
 			break;
 		case 0x14: // 5520 "E314" "LGF" "RXY"
@@ -8582,6 +8649,7 @@ public class pz390 {
 			reg.put(rf1+4,(byte)(reg.get(rf1+4) & 0x7f)); // turn off high 31 bit
 			break;
 		case 0x49: // "E349=STGSC,18,180", // E349 RXYa STGSC R1,D2(X2,B2) RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rxy();
 			break;
 		case 0x4C: // "E34C=LGG,18,180", // E34C RXYa LGG R1,D2(X2,B2) RPI 2202
@@ -8592,6 +8660,7 @@ public class pz390 {
 			reg.put(rf1+4,(byte)(reg.get(rf1+4) & 0x7f)); // turn off high 31 bit 
 			break;
 		case 0x4D: // "E34D=LGSC,18,180", // E34D RXYa LGSC R1,D2(X2,B2) RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rxy();
 			break;
 		case 0x50: // 5750 "E350" "STY" "RXY"
@@ -9081,12 +9150,15 @@ public class pz390 {
     	opcode2 = mem_byte[psw_loc + opcode2_offset_sse] & 0xff;
 		switch (opcode2) {
 		case 0x00: // 6120 "E500" "LASP" "SSE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_sse();
 			break;
 		case 0x01: // 6130 "E501" "TPROT" "SSE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_sse();
 			break;
 		case 0x02: // 6140 "E502" "STRAG" "SSE"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_sse();
 			break;
 		case 0x0A: // E50A MVCRL,19,192 RPI 2202
@@ -9154,9 +9226,11 @@ public class pz390 {
 			psw_cc = get_int_log_comp_cc(mem.getInt(bd1_loc), if2 & 0xffff);
 		    break;
 	     case 0x60:  // E560 SIL TBEGIN D1(B1),I2 RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_sil();
 		    break;
 	     case 0x61:  // E561 SIL TBEGINC D1(B1),I2 RPI 2202
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_sil();
 		    break;
 		}
@@ -9212,6 +9286,7 @@ public class pz390 {
 			reg.putLong(rf1, reg.getLong(rf3) << (bd2_loc & 0x3f));
 			break;
 		case 0x0F: // 6250 "EB0F" "TRACG" "RSY"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rsy();
 			break;
 		case 0x14: // 6260 "EB14" "CSY" "RSY"
@@ -9265,6 +9340,7 @@ public class pz390 {
 			}
 			break;
 		case 0x25: // 6320 "EB25" "STCTG" "RSY"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rsy();
 			break;
 		case 0x26: // 6330 "EB26" "STMH" "RSY"
@@ -9297,6 +9373,7 @@ public class pz390 {
 			exec_stcm();
 			break;
 		case 0x2F: // 6360 "EB2F" "LCTLG" "RSY"
+			set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 			ins_setup_rsy();
 			break;
 		case 0x30: // 6370 "EB30" "CSG" "RSY"
@@ -11338,6 +11415,7 @@ public class pz390 {
 	 * ********************************************
 	 */
 	private void ins_setup_dm() { // "DM" 1 DIAGNOSE 83000000
+		set_psw_check(psw_pic_priv); // RPI 1622 DK Privileged operation exception
 		psw_ins_len = 4;
 		if (tz390.opt_trace) {
 			trace_ins();
