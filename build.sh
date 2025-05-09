5b9c5ac2fb8ca168b56484c3a0ff80010c8f4cd6
Anthony Delosa <adelosa@gmail.com>
2025-05-09 20:08:18 +1000

    Build dist before tests

--- build.sh
+++ build.sh
@@ -7,14 +7,18 @@ then
     git clean -dfX              # delete all git ignored files
 fi
 
-# build the jar and required libs
-bash/bldjar $1
-bash/bldzstrmac
-bash/bldlib
-bash/bldcbllib
+#!/bin/bash
+set -e
+echo "::set-output name=javaversion::$(java -version)"
 
-# run the tests
-z390test/gradlew -p z390test test
+if [ "$1" == "clean" ];         # if "clean" parm passed
+then
+    git clean -dfX              # delete all git ignored files
+fi
 
 # build the package
-bash/blddist
+bash/blddist $1
+
+# run the tests
+z390test/gradlew -p z390test cleanTest
+z390test/gradlew -p z390test test
