d656378e3c4beafbf1d170079642ff283ac4dcc5
Anthony Delosa <adelosa@gmail.com>
2025-05-10 16:20:36 +1000

    Remove all git from build

--- build.sh
+++ build.sh
@@ -2,22 +2,8 @@
 set -e
 echo "::set-output name=javaversion::$(java -version)"
 
-if [ "$1" == "clean" ];         # if "clean" parm passed
-then
-    git clean -dfX              # delete all git ignored files
-fi
-
-#!/bin/bash
-set -e
-echo "::set-output name=javaversion::$(java -version)"
-
-if [ "$1" == "clean" ];         # if "clean" parm passed
-then
-    git clean -dfX              # delete all git ignored files
-fi
-
 # build the package
-bash/blddist $1
+bash/blddist "$@"
 
 # run the tests
 z390test/gradlew -p z390test cleanTest
