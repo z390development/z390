b195be98958132c33f0ee97a8486116f30e406cb
Anthony Delosa <adelosa@gmail.com>
2025-05-18 18:14:24 +1000

    Fixes to new demo tests

--- z390test/src/test/groovy/org/z390/test/RunAsmDemos.groovy
+++ z390test/src/test/groovy/org/z390/test/RunAsmDemos.groovy
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test
 
 class RunAsmDemos extends z390Test {
 
-    var options = ['trace', 'noloadhigh', "SYSMAC(${basePath("mac")})"]
+    var options = [
+        'trace',
+        'noloadhigh', 
+        "SYSMAC(${basePath('mac')})",
+        "SYSCPY(${basePath('mac')})",
+        "SYSOBJ(${basePath('linklib')})"
+    ]
 
     @Test
     void test_HELLO() {
@@ -18,46 +24,55 @@ class RunAsmDemos extends z390Test {
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMOBMK1() {
-        int rc = this.asmlg(basePath("demo", "DEMOBMK1"), *options)
+        int rc = this.asmlg(basePath("demo", "DEMOBMK1"), *options, "MAXSIZE(150)")
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMOBMK2() {
         int rc = this.asmlg(basePath("demo", "DEMOBMK2"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMODFP1() {
         int rc = this.asmlg(basePath("demo", "DEMODFP1"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMONUM1() {
         int rc = this.asm(basePath("demo", "DEMONUM1"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMONUM2() {
         int rc = this.asmlg(basePath("demo", "DEMONUM2"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMOSTR1() {
         int rc = this.asmlg(basePath("demo", "DEMOSTR1"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_DEMOWTO2() {
-        int rc = this.asmlg(basePath("demo", "DEMOSTO2"), *options)
+        int rc = this.asmlg(basePath("demo", "DEMOWTO2"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_SIEVE() {
         int rc = this.asmlg(basePath("demo", "SIEVE"), *options)
         this.printOutput()
         assert rc == 0
     }
+    @Test
     void test_STDDEVLB() {
         int rc = this.asmlg(basePath("demo", "STDDEVLB"), *options)
         this.printOutput()
