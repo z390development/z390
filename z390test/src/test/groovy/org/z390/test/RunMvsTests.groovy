4a8b7abec726f9df2eb456976e906fe3f85ed78d
Abe Kornelis <abe@bixoft.nl>
2026-05-29 15:12:47 +0200

    Revive test programs for the mvs subfolder

---
+++ z390test/src/test/groovy/org/z390/test/RunMvsTests.groovy
@@ -0,0 +1,38 @@
+package org.z390.test
+
+import org.junit.jupiter.api.Test
+
+class RunMvsTests extends z390Test {
+
+    var options = ['notiming', 'stats', 'bal', 'noloadhigh']
+    var sysmacAddMvs = "SYSMAC(+${basePath('mvs', 'maclib')})"
+    var sysmacMvsAndMac = "SYSMAC(${basePath('mvs', 'maclib')}+${basePath('mac')})"
+
+    @Test
+    void test_DEMO() {
+        int rc = this.asmlg(basePath('mvs', 'demo', 'DEMO'), *options)
+        this.printOutput()
+        assert rc == 0
+    }
+
+    @Test
+    void test_CVTX() {
+        int rc = this.asm(basePath('mvs', 'test', 'TESTCVTX'), *options)
+        this.printOutput()
+        assert rc == 0
+    }
+
+    @Test
+    void test_CVT1() {
+        int rc = this.asmlg(basePath('mvs', 'test', 'TESTCVT1'), *options, sysmacAddMvs)
+        this.printOutput()
+        assert rc == 0
+    }
+
+    @Test
+    void test_MVS1() {
+        int rc = this.asmlg(basePath('mvs', 'test', 'TESTMVS1'), *options, sysmacMvsAndMac)
+        this.printOutput()
+        assert rc == 0
+    }
+}
