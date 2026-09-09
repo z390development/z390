280fe855ef0ebbdd29864582482c157048ba9295
Bob <rjs39@hotmail.com>
2026-09-09 16:47:02 -0400

    First round of az390 changes to address the error line issues.

--- src/az390.java
+++ src/az390.java
@@ -5947,7 +5947,50 @@ public  class  az390 implements Runnable {
         }
         loc_ctr = loc_ctr + loc_len;
     }
-
+   /**
+    * list bal line with first 8 bytes of
+    * object code if any 
+    * and turn off list_bal_line request
+    * <br />
+    * Note:
+    *    See comments processing case 0
+    *    for update of mac_call_gen,
+    *    call reformating, and delay flags
+    *    mac_call_first and mac_call_last.
+    * This routine is a clone of list_bal_line but
+    * excludes the PC and object code from the listing.   RPI 877
+    * If you change this routine, change list_bal_line() too.
+    */
+   private void list_bal_line_error(){
+       if (!check_list_bal_line()){ // RPI 484 RPI 891
+           update_list_bal_line();
+    	   return;
+       }
+       if (list_obj_code.length() < 16){
+          list_obj_code = list_obj_code.concat("                ").substring(0,16);
+       } 
+       list_obj_loc = loc_start;
+       if (gen_obj_code){ // RPI 581
+    	  cur_line_type     = xref_file_type[bal_line_xref_file_num[bal_line_index]];
+    	  cur_line_file_num = bal_line_xref_file_num[bal_line_index];
+    	  put_prn_line("      "       // Do not diplay the PC on error lines  RPI 877
+		    + " " + list_obj_code.substring(0,16) 
+		    + " " + hex_bddd1_loc 
+		    + " " + hex_bddd2_loc 
+		    + " " + tz390.get_cur_bal_line_id(cur_line_file_num,
+                  bal_line_xref_file_line[bal_line_index],
+                  bal_line_num[bal_line_index],
+                  mac_call_gen, // RPI 891 
+                  cur_line_type) 
+		    + bal_line);
+       }
+       force_list_bal = false;   // RPI 285
+       update_list_bal_line();
+       if (list_use){
+    	  list_use();
+    	  list_use = false;
+       }
+    }
 
 
     /**
@@ -5960,6 +6003,7 @@ public  class  az390 implements Runnable {
      *    for update of mac_call_gen,
      *    call reformating, and delay flags
      *    mac_call_first and mac_call_last.
+     * If you change this routine, change list_bal_line_error() too.
      */
     private void list_bal_line() {
         if (!check_list_bal_line()) { // RPI 484 RPI 891
@@ -8314,7 +8358,10 @@ public  class  az390 implements Runnable {
         if (gen_obj_code) { // RPI 484
             if (!mz390_abort) { // RPI 433 don't duplicate mz error line
                 force_list_bal = true;  // RPI 285
-                list_bal_line();
+                list_obj_code = "";             // RPI 877
+                hex_bddd1_loc = "      ";       // RPI 877
+                hex_bddd2_loc = "      ";       // RPI 877
+			    list_bal_line_error();          // RPI 877
             }
             force_list_bal = true;  // RPI 285
             set_file_line_xref();
